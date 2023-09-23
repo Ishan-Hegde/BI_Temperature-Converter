64f7d91822ecfdd1cf8428d55b5516dfac3536f5
IshanFelix <hegdeishan10@gmail.com>
2023-09-24 02:46:37 +0530

    finally done...all set

--- app.js
+++ app.js
@@ -20,20 +20,20 @@ const swap = () => {
 
 const reset = () => {
     converter.value = '';
-    result.innerHTML = '';
+    result.innerHTML = 'Result:';
 }
 
 const convert = () => {
     if (/^(-?)(\d+)(\.{1}\d+)?$/.test(converter.value)) {
         if (C.innerHTML === '°C') {
             let value = (converter.value * 1.8) + 32;
-            result.innerHTML = `${converter.value}°C is equal to ${value.toFixed(2)}°F`;
+            result.innerHTML = `Result: ${converter.value}°C is equal to ${value.toFixed(2)}°F`;
         } else {
             let value = (converter.value - 32) / 1.8;
-            result.innerHTML = `${converter.value}°F is equal to ${value.toFixed(2)}°C`;
+            result.innerHTML = `Result: ${converter.value}°F is equal to ${value.toFixed(2)}°C`;
         }
     } else if (converter.value == '') {
-        result.innerHTML = 'Enter some number';
+        result.innerHTML = 'Enter some number!!!';
     } else {
         result.innerHTML = 'Enter only numbers!!!';
     }
@@ -42,3 +42,64 @@ const convert = () => {
 changeButton.addEventListener('click', swap);
 resetButton.addEventListener('click', reset);
 convertButton.addEventListener('click', convert);
+
+
+
+// TIME
+window.addEventListener("load", () => {
+    clock();
+    function clock() {
+      const today = new Date();
+  
+      // get time components
+      const hours = today.getHours();
+      const minutes = today.getMinutes();
+      const seconds = today.getSeconds();
+  
+      //add '0' to hour, minute & second when they are less 10
+      const hour = hours < 10 ? "0" + hours : hours;
+      const minute = minutes < 10 ? "0" + minutes : minutes;
+      const second = seconds < 10 ? "0" + seconds : seconds;
+  
+      //make clock a 12-hour time clock
+      const hourTime = hour > 12 ? hour - 12 : hour;
+  
+      // if (hour === 0) {
+      //   hour = 12;
+      // }
+      //assigning 'am' or 'pm' to indicate time of the day
+      const ampm = hour < 12 ? "AM" : "PM";
+  
+      // get date components
+      const month = today.getMonth();
+      const year = today.getFullYear();
+      const day = today.getDate();
+  
+      //declaring a list of all months in  a year
+      const monthList = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+  
+      //get current date and time
+      const date = monthList[month] + " " + day + ", " + year;
+      const time = hourTime + ":" + minute + ":" + second + ampm;
+  
+      //combine current date and time
+      const dateTime = date + " - " + time;
+  
+      //print current date and time to the DOM
+      document.getElementById("date-time").innerHTML = dateTime;
+      setTimeout(clock, 1000);
+    }
+  });
